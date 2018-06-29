eef5eb5aca9469299456d77ce491044346b8f942
Jesse Adler <jesseadler44@gmail.com>
2018-06-29 11:15:04 +0100

    Exercise-1

--- script/app.js
+++ script/app.js
@@ -1,1 +1,13 @@
-/* Write your code here! */
+$(document).ready(function(){
+    $(".announcement").append("<button class='button'>X</button>");
+   
+    $(".announcement button").click(function(){
+      $(".announcement p").toggle();
+      if ($(".announcement button").html() == "X"){
+        $("button").text("Show announcement");
+      }
+      else{
+        $("button").text("X");
+      }
+      });
+   });
